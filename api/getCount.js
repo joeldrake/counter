eec0339ba596220a1464723121ef7b6969f89640
Joel Drake <joeldrak3@outlook.com>
2023-02-07 21:06:22 +0100

    date test

--- api/getCount.js
+++ api/getCount.js
@@ -1,17 +1,30 @@
+/**
+ * @param {import('@vercel/node').VercelRequest} request
+ * @param {import('@vercel/node').VercelResponse} response
+ */
 export default function handler(request, response) {
+  // const MY_SECRET = process.env.MY_SECRET
+
+  const headers = request?.headers;
+
+  /**
+   * @type {Intl.DateTimeFormatOptions}
+   */
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
+
+  const date = new Date().toLocaleDateString("sv-SE", options);
+
   const object = {
-    asd: process.env.MY_SECRET,
+    headers,
+    date,
     link1: 1,
     link2: 2,
     link3: 3,
     link4: 4,
   };
   response.status(200).json(object);
-
-  //   {
-  //     body: request.body,
-  //     query: request.query,
-  //     cookies: request.cookies,
-  //     env: process.env.MY_SECRET,
-  //   }
 }
